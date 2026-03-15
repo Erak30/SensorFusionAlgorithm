ce9167956516eb428ca0e1f249b1dfa401721c10
Erak30 <47530257+Erak30@users.noreply.github.com>
2026-03-16 00:00:05 +0100

    Add files via upload

---
+++ imu/imu.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  mpu.initialize();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+  Serial.print(ax); Serial.print(",");
+  Serial.print(ay); Serial.print(",");
+  Serial.print(az); Serial.print(",");
+  Serial.print(gx); Serial.print(",");
+  Serial.print(gy); Serial.print(",");
+  Serial.println(gz);
+
+  delay(1000);
+}
